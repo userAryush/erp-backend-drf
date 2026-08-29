10758ca9a29394dcca22480b1bf012b694eaf196
userAryush <khatriaryush@gmail.com>
2026-08-29 15:02:23 +0545

    feat(auth): add custom user model and user manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,30 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from Base.models import BaseModel
+from managers import UserManager
 
+class Role(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
 
-class User(AbstractUser):
-    pass
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Role"
+        verbose_name_plural = "Roles"
+
+    def __str__(self):
+        return self.name
+    
+class User(AbstractUser, BaseModel):
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.PROTECT,
+        related_name="users",
+    )    
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['role']
+    
+    objects = UserManager()
+
