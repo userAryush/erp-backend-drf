e3ef92d6e6a5c1c3b1af27f0dc006ff61f432368
userAryush <khatriaryush@gmail.com>
2026-08-30 12:11:11 +0545

    feat(auth) : fix models conflict

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from Base.models import BaseModel
-from managers import UserManager
+from .managers import UserManager
 
 class Role(BaseModel):
     name = models.CharField(max_length=50, unique=True)
