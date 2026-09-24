1c381188138cf32fa74cbd92b6b8bfb3ce2dab48
userAryush <khatriaryush@gmail.com>
2026-09-24 18:43:52 +0545

    feat(warehouse) : add models and migrations

--- apps/warehouse/apps.py
+++ apps/warehouse/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class WarehouseConfig(AppConfig):
-    name = 'warehouse'
+    name = 'apps.warehouse'
--- apps/warehouse/models.py
+++ apps/warehouse/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from Base.models import BaseModel
+
+
+class Warehouse(BaseModel):
+    name = models.CharField(max_length=150)
+    code = models.CharField(max_length=50, unique=True)
+    address = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
